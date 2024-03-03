d97fa69026b673c422448636639e6384d89d41fd
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-03-03 21:16:53 +0530

    done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,8 +9,8 @@ const Navbar = () => {
   <div class="max-w-screen-xl flex justify-center mx-auto">
     <div className='fixed w-full bg-gray-900 flex justify-between mx-auto p-4 z-10'>
       <NavLink to="/">
-        <a class="flex items-center space-x-3 rtl:space-x-reverse relative md:ml-16 ml-6">
-            <img src={nitlogo} class="h-24 absolute left-[-20%]" />
+        <a class="flex items-center space-x-3 rtl:space-x-reverse relative md:ml-20 ml-9">
+            <img src='https://media.licdn.com/dms/image/C4D0BAQGqY_k8w5ZZlw/company-logo_200_200/0/1659076719111?e=2147483647&v=beta&t=0KLCPblrqtRIPJobI2EfEpNzUULjqvsANM1axILuQ5c' class="h-12 absolute left-[-20%]" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SAE NIT Kurukshetra</span>
         </a>
       </NavLink>
