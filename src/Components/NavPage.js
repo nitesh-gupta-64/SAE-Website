b78342086b3a0d4ee9ae84bbe133b164f8df7f3a
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-03-05 12:03:08 +0530

    Updated

--- src/Components/NavPage.js
+++ src/Components/NavPage.js
@@ -6,11 +6,11 @@ const NavPage = () => {
     <div className='flex justify-center items-center min-h-screen overflow-hidden'>
         <div className="w-full block md:w-auto z-10" id="n">
             <ul>
-                <li><a href="/" data-text="Home">Home</a></li>
-                <li><a href="/autokriti" data-text="Autokriti">Autokriti</a></li>
-                <li><a href="/accelerons" data-text="Accelerons">Accelerons</a></li>
-                <li><a href="/nitrox" data-text="Nitrox">Nitrox</a></li>
-                <li><a href="/sponsors" data-text="Sponsors">Sponsors</a></li>
+                <li><a className=' before:lg:text-[4em] before:md:text-[3em] before:text-[1em]' href="/" data-text="Home">Home</a></li>
+                <li><a className=' before:lg:text-[4em] before:md:text-[3em] before:text-[1em]' href="/autokriti" data-text="Autokriti">Autokriti</a></li>
+                <li><a className=' before:lg:text-[4em] before:md:text-[3em] before:text-[1em]' href="/accelerons" data-text="Accelerons">Accelerons</a></li>
+                <li><a className=' before:lg:text-[4em] before:md:text-[3em] before:text-[1em]' href="/nitrox" data-text="Nitrox">Nitrox</a></li>
+                <li><a className=' before:lg:text-[4em] before:md:text-[3em] before:text-[1em]' href="/sponsors" data-text="Sponsors">Sponsors</a></li>
             </ul>
         </div>
     </div>
